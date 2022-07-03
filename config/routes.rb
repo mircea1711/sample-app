a687f0026524d526ed55c9c68fa3b45c86c8429c
mircea <mircea.maierean@yahoo.com>
2022-07-03 23:51:08 +0000

    Add user microposts

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   get 'password_resets/new'
   get 'password_resets/edit'
   get 'sessions/new'
-  resources :users
   root 'static_pages#home'
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
@@ -12,6 +11,8 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login',   to: 'sessions#create'
   delete '/logout',   to: 'sessions#destroy'
+  resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
+  resources :microposts, only: [:create, :destroy]
 end
